6485128eb75480859bca5f6273acc3435a7e0b11
judaicadhpenn <157063908+judaicadhpenn@users.noreply.github.com>
2025-08-20 17:54:03 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -36,7 +36,7 @@ export default defineConfig({
 
 
 
-          customCss: [ './src/styles/global.css',
+          customCss: [ './src/styles/starlight.css',
           ],
           favicon: './assets/images/favicon.ico',
 
